8fb82fc5b799a039ef67a62f863dae10a47a50c8
elchibek <elchi.work00@gmail.com>
2024-05-02 20:01:54 +0600

    Add project files.

---
+++ ToDoApp/Models/TodoModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ToDoApp.Models
+{
+    internal class TodoModel : INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+		private bool _isDone;
+        private string _text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsDone
+        {
+			get { return _isDone; }
+			set 
+			{
+				if (_isDone == value)
+					return;
+				_isDone = value; 
+				OnPropertyChanged("IsDone");
+			}
+		}
+
+		public string Text
+		{
+			get { return _text; }
+			set 
+			{ 
+				if (_text == value)
+					return;
+				_text = value;
+                OnPropertyChanged("Text");
+            }
+		}
+
+		protected virtual void OnPropertyChanged(string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
